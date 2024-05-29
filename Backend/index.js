a8dab4754dec35939a44abf0ebb8f5cb6a2d3571
yogesh9949 <yy2749564@gmail.com>
2024-05-29 13:57:24 +0530

    app completed

--- Backend/index.js
+++ Backend/index.js
@@ -3,10 +3,11 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
+import { dir } from "console";
+import path from "path";
 
 dotenv.config();
 
@@ -15,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4001;
 const URI = process.env.MONGODB_URI;
 
 try {
@@ -28,6 +29,18 @@ try {
 //routes
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
+app.use("/api/users", userRoute);
+
+//..................code for deployment...........
+
+if(process.env.NODE_ENV === "production"){
+    const dirpath = path.resolve();
+
+    app.use(express.static("./Frontend/dist"));
+    app.get("*",(req,res) =>{
+        res.sendFile(path.resolve(dirpath , "./Frontend/dist","index.html"));
+    })
+}
 
 server.listen(PORT, () => {
     console.log(`Server is Running on port ${PORT}`);
